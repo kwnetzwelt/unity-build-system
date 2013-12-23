cf2eff275c3a7e193de7c9d246a5a1de310f6028
Kai Wegner <kwnetzwelt@gmail.com>
2013-12-23 20:04:41 +0100

    comment on tooltip functionality in description attribute

--- unity/unity-build-system/Assets/UBS/Editor/UBS/BuildStepDescriptionAttribute.cs
+++ unity/unity-build-system/Assets/UBS/Editor/UBS/BuildStepDescriptionAttribute.cs
@@ -4,6 +4,7 @@ namespace UBS
 {
 	/// <summary>
 	/// Use this attribute to add a custom description to a build step. 
+	/// It is displayed as a tooltip, when the build step is selected and you hover the dropdown box. 
 	/// </summary>
 	public class BuildStepDescriptionAttribute : System.Attribute
 	{
