d0fd6ef5b037f458e8998efe4e2bce270c2eb308
Kai Wegner <kwnetzwelt@gmail.com>
2014-04-22 17:31:01 +0200

    added generic compiler flag add/remove step

---
+++ unity/unity-build-system/Assets/UBS/Editor/Steps/RemoveCompilerFlag.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using UBS;
+using UnityEditor;
+
+[BuildStepDescriptionAttribute("Removes a compiler flag to your build.")]
+
+public class RemoveCompilerFlag : IBuildStepProvider
+{
+	
+	#region IBuildStepProvider implementation
+	
+	public void BuildStepStart (BuildConfiguration pConfiguration)
+	{
+		BuildTargetGroup btg = UBS.Helpers.GroupFromBuildTarget( pConfiguration.GetCurrentBuildProcess().mPlatform );
+		string symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(btg);
+		List<string> symbolsArray = new List<string>(symbols.Split(';'));
+		
+		if(symbolsArray.Contains(pConfiguration.Params))
+			symbolsArray.Remove(pConfiguration.Params);
+		symbols = string.Join(";", symbolsArray.ToArray());
+		
+		
+		PlayerSettings.SetScriptingDefineSymbolsForGroup(btg, symbols);
+	}
+	
+	public void BuildStepUpdate ()
+	{
+		
+	}
+	
+	public bool IsBuildStepDone ()
+	{
+		return true;
+	}
+	
+	#endregion
+}
+
