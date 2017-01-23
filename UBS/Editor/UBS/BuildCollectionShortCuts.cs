c39bf5b0383b78b61e8005238c036d8b28b01b8b
Kai Wegner <kai.wegner@exozet.com>
2017-01-23 13:07:14 +0100

    The most recent build collection can now be selected by hitting CTRL+SHIFT+C

---
+++ UBS/Editor/UBS/BuildCollectionShortCuts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace UBS
+{
+    public class BuildCollectionShortCuts
+    {
+
+        [MenuItem("Window/UBS/Select Last Build Collection #%C", true)]
+        static bool BuildCollectionAvailable()
+        {
+            return GetLastBuildCollection() != null;
+        }
+
+        [MenuItem("Window/UBS/Select Last Build Collection #%C")]
+        public static void ShowLastBuildCollection()
+        {
+            BuildCollection lastBuildcollection = GetLastBuildCollection();
+            EditorGUIUtility.PingObject(lastBuildcollection);
+            Selection.activeObject = lastBuildcollection;
+        }
+
+        private static BuildCollection GetLastBuildCollection()
+        {
+            var process = UBSProcess.LoadUBSProcess();
+            if (process != null)
+                return process.BuildCollection;
+            return null;
+        }
+    }
+}
