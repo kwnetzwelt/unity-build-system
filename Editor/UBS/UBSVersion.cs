64460b862b1d0e37302e646cf2265a4d0d687b4d
Kai Wegner <kwnetzwelt@gmail.com>
2022-06-24 09:14:30 +0200

    updates version to 1.2.1

--- Editor/UBS/UBSVersion.cs
+++ Editor/UBS/UBSVersion.cs
@@ -7,6 +7,6 @@ namespace UBS
 {
     public static class UBSVersion
     {
-        public static readonly System.Version version = new System.Version(1, 2, 0);
+        public static readonly System.Version version = new System.Version(1, 2, 1);
     }
 }
