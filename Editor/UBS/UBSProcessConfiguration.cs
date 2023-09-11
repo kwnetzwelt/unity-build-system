a1d0fa2e48e2b9af41b4059706227d1ab93d77e6
Kai Wegner <kwnetzwelt@gmail.com>
2023-09-11 10:12:59 +0200

    add missing serializeField Attributes

--- Editor/UBS/UBSProcessConfiguration.cs
+++ Editor/UBS/UBSProcessConfiguration.cs
@@ -1,20 +1,30 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace UBS
 {
     [Serializable]
     public class UBSProcessConfiguration
     {
+        [field: SerializeField]
         public CommandLineArgsParser.ArgsCollection CommandlineArgs { get; set; } = new();
+        [field: SerializeField]
         public List<string> SelectedBuildProcessNames { get; set; } = new();
+        [field: SerializeField]
         public BuildCollection Collection { get; set; }
+        [field: SerializeField]
         public bool BuildAndRun { get; set; }
+        [field: SerializeField]
         public bool BatchMode { get; set; }
+        [field: SerializeField]
         public bool BuildAll { get; set; }
+        [field: SerializeField]
         public string BuildTag { get; set; }
+        [field: SerializeField]
         public CleanBuildArgument Clean { get; set; }
 
+        [field: SerializeField]
         public List<BuildProcess> SelectedBuildProcesses { get; set; } = new();
     }
 }
